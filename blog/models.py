4a93290ec3155453814c3ff52dbc8b244472d4ba
Marina Machado <marina.fmachado@usp.br>
2019-06-07 10:22:37 -0300

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)#link para outro modelo
+    title = models.CharField(max_length=200)#texto com tamanho limitado
+    text = models.TextField()#texto com tamanho ilimitado
+    createdDate = models.DateTimeField(#data e hora
+            default=timezone.now)
+    publishedDate = models.DateTimeField(
+            blank=True, null=True)
+
+    def publicacao(self): #metodo
+        self.publishedDate = timezone.now()
+        self.save()
+
+    def __getTitle___(self):
+        return self.title
